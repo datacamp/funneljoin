ffea803e713821f73d3b0795a803518f4a01e24a
Emily Robinson <emily@datacamp.com>
2018-07-23 17:06:50 -0400

    add tests first firstafter

--- tests/testthat/test_first_first_after.R
+++ tests/testthat/test_first_first_after.R
@@ -40,3 +40,38 @@ test_that("after_join works with mode = inner and type = first-firstafter", {
   expect_true(all(!is.na(res$timestamp.y)))
   expect_true(all(!is.na(res$user_id)))
 })
+
+test_that("after_join works with mode = left and type = first-firstafter", {
+
+  res <- after_join(landed, registered, by_user = "user_id", by_time = c("timestamp" = "timestamp"), mode = "left", type = "first-firstafter")
+
+  expect_is(res, "tbl_df")
+  expect_equal(names(res), c("user_id", "timestamp.x", "timestamp.y"))
+  expect_true(all(res$timestamp.y >= res$timestamp.x |
+                    is.na(res$timestamp.y)))
+  expect_equal(length(res$user_id), n_distinct(res$user_id))
+  expect_gt(nrow(landed), nrow(res))
+  expect_true(4 %in% res$user_id)
+  expect_true(1 %in% res$user_id)
+  expect_true(all(!is.na(res$timestamp.x)))
+  expect_true(any(is.na(res$timestamp.y)))
+  expect_true(all(!is.na(res$user_id)))
+})
+
+test_that("after_join works with mode = right and type = first-firstafter", {
+
+  res <- after_join(landed, registered, by_user = "user_id", by_time = c("timestamp" = "timestamp"), mode = "right", type = "first-firstafter")
+
+  expect_is(res, "tbl_df")
+  expect_equal(names(res), c("user_id", "timestamp.x", "timestamp.y"))
+  expect_true(all(res$timestamp.y >= res$timestamp.x |
+                    is.na(res$timestamp.x)))
+  expect_equal(length(res$user_id), n_distinct(res$user_id))
+  expect_equal(nrow(res), nrow(registered))
+  expect_true(4 %in% res$user_id)
+  expect_true(1 %in% res$user_id)
+  expect_true(!2 %in% res$user_id)
+  expect_true(all(!is.na(res$timestamp.y)))
+  expect_true(any(is.na(res$timestamp.x)))
+  expect_true(all(!is.na(res$user_id)))
+})
