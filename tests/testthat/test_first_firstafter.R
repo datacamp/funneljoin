a060abb9199d4661a5ecdb54a1affff5593506e0
Emily Robinson <emily@datacamp.com>
2018-07-23 17:24:10 -0400

    finish tests first-firstafter

--- tests/testthat/test_first_firstafter.R
+++ tests/testthat/test_first_firstafter.R
@@ -12,7 +12,8 @@ landed <- tribble(
   5, "2018-07-12",
   6, "2018-07-07",
   6, "2018-07-08"
-)
+) %>%
+  mutate(timestamp = as.Date(timestamp))
 
 registered <- tribble(
   ~user_id, ~timestamp,
@@ -24,8 +25,8 @@ registered <- tribble(
   6, "2018-07-10",
   6, "2018-07-11",
   7, "2018-07-07"
-)
-
+) %>%
+  mutate(timestamp = as.Date(timestamp))
 test_that("after_join works with mode = inner and type = first-firstafter", {
 
   res <- after_join(landed, registered, by_user = "user_id", by_time = c("timestamp" = "timestamp"), mode = "inner", type = "first-firstafter")
@@ -75,3 +76,47 @@ test_that("after_join works with mode = right and type = first-firstafter", {
   expect_true(any(is.na(res$timestamp.x)))
   expect_true(all(!is.na(res$user_id)))
 })
+
+test_that("after_join works with mode = anti and type = first-firstafter", {
+
+  res <- after_join(landed, registered, by_user = "user_id", by_time = c("timestamp" = "timestamp"), mode = "anti", type = "first-firstafter")
+
+  expect_is(res, "tbl_df")
+  expect_equal(names(res), c("user_id", "timestamp"))
+  expect_true(2 %in% res$user_id)
+  expect_true(!3 %in% res$user_id)
+  expect_true(all(!is.na(res$timestamp)))
+  expect_true(all(!is.na(res$user_id)))
+  expect_true(as.Date("2018-07-01") %in% res$timestamp)
+})
+
+test_that("after_join works with mode = semi and type = first-firstafter", {
+
+  res <- after_join(landed, registered, by_user = "user_id", by_time = c("timestamp" = "timestamp"), mode = "semi", type = "first-firstafter")
+
+  expect_is(res, "tbl_df")
+  expect_equal(names(res), c("user_id", "timestamp"))
+  expect_true(!2 %in% res$user_id)
+  expect_true(3 %in% res$user_id)
+  expect_true(all(!is.na(res$timestamp)))
+  expect_true(all(!is.na(res$user_id)))
+  expect_true(as.Date("2018-07-10") %in% res$timestamp)
+})
+
+test_that("after_join works with mode = full and type = first-firstafter", {
+
+  res <- after_join(landed, registered, by_user = "user_id", by_time = c("timestamp" = "timestamp"), mode = "full", type = "first-firstafter")
+
+  expect_is(res, "tbl_df")
+  expect_equal(names(res), c("user_id", "timestamp.x", "timestamp.y"))
+  expect_true(nrow(res) >= 8)
+  expect_true(all(res$timestamp.y >= res$timestamp.x |
+                    is.na(res$timestamp.x) |
+                    is.na(res$timestamp.y)))
+  expect_gt(nrow(res), n_distinct(landed$user_id))
+  expect_gt(nrow(res), n_distinct(registered$user_id))
+  expect_true(any(is.na(res$timestamp.x)))
+  expect_true(any(is.na(res$timestamp.y)))
+  expect_true(all(!is.na(res$user_id)))
+})
+
