449de010b10c330447b289065b833a816e3f0eea
Emily Robinson <emily@datacamp.com>
2018-08-09 14:16:55 -0400

    make distinct events work with remote tables

--- R/distinct_events.R
+++ R/distinct_events.R
@@ -1,6 +1,6 @@
-#' Title
+#' Distinct events
 #'
-#' @param .data
+#' @param .data a dataset, either local or remote
 #' @param time_col the name of the time column
 #' @param user_col the name of the user identifying column
 #' @param type the type of after_join ("first-first", "first-firstafter", etc.)
@@ -9,26 +9,25 @@
 #'
 #' @examples
 distinct_events <- function(.data, time_col, user_col, type) {
-
-  if (type == "first") {
-    data_sorted <- .data %>%
-      arrange(!!sym(time_col))
-  } else if (type == "last") {
-    data_sorted <- .data %>%
-      arrange(desc(!!sym(time_col)))
-  }
-
   if (inherits(.data, "tbl_lazy")) {
-    ret <- data_sorted %>%
-      group_by(!!sym(user_col)) %>%
-      mutate(..rank = row_number()) %>%
-      ungroup() %>%
+    desc <- if (type == "first") " DESC" else ""
+    rank_sql <- sql(glue::glue('RANK() OVER (PARTITION BY "{ user_col }" ORDER BY "{ time_col }" { desc })'))
+
+    ret <- .data %>%
+      mutate(..rank = rank_sql) %>%
       filter(..rank == 1) %>%
       select(-..rank)
   } else {
+    if (type == "first") {
+      data_sorted <- .data %>%
+        arrange(!!sym(time_col))
+    } else if (type == "last") {
+      data_sorted <- .data %>%
+        arrange(desc(!!sym(time_col)))
+    }
+
     ret <- data_sorted %>%
       distinct(!!sym(user_col), .keep_all = T)
   }
-
   ret
 }
