cb23ddc4aa5ebb8fad3ee7a9b844e618f578c71c
Emily Robinson <emily@datacamp.com>
2018-08-10 12:01:45 -0400

    add remote tests for gap joins

--- tests/testthat/test_remote.R
+++ tests/testthat/test_remote.R
@@ -16,7 +16,8 @@ test_that("after_join works for out-of-memory tables with mode = inner and type 
   res <- after_join(soft_launches,
                     hard_launches,
                     by_user = "course_id",
-                    by_time = c("soft_launch_at" = "live_at")) %>%
+                    by_time = c("soft_launch_at" = "live_at"),
+                    type = "first-first") %>%
     dplyr::collect()
 
   expect_is(res, "tbl_df")
@@ -114,3 +115,92 @@ test_that("after_join works for out-of-memory tables with mode = semi and type =
 
   expect_equal(n_distinct(hard_launches_in_res$course_id), nrow(res))
 })
+
+test_that("after_join works for out-of-memory tables with mode = inner and type = smallestgap", {
+
+  res <- after_join(soft_launches,
+                    hard_launches,
+                    by_user = "course_id",
+                    by_time = c("soft_launch_at" = "live_at"),
+                    type = "smallestgap") %>%
+    dplyr::collect()
+
+  expect_is(res, "tbl_df")
+  expect_equal(names(res), c("course_id", "soft_launch_at", "live_at"))
+  expect_true(all(res$soft_launch_at <= res$live_at))
+  expect_equal(length(res$course_id), dplyr::n_distinct(res$course_id))
+  expect_true(nrow(res) >= 80)
+  expect_true(all(!is.na(res$soft_launch_at)))
+  expect_true(all(!is.na(res$live_at)))
+  expect_true(all(!is.na(res$course_id)))
+
+  first_soft_launches <- soft_launches %>%
+    dplyr::group_by(course_id) %>%
+    dplyr::summarise(first_soft_launch_at = min(soft_launch_at, na.rm = T)) %>%
+    dplyr::collect() %>%
+    inner_join(res, by = "course_id")
+
+  expect_equal(first_soft_launches$first_soft_launch_at, first_soft_launches$soft_launch_at)
+})
+
+test_that("after_join works for out-of-memory tables with mode = inner and type = withingap with difftime", {
+
+  res <- after_join(soft_launches,
+                    hard_launches,
+                    by_user = "course_id",
+                    by_time = c("soft_launch_at" = "live_at"),
+                    type = "withingap",
+                    dt = as.difftime(10, unit = "days")) %>%
+    dplyr::collect()
+
+  expect_is(res, "tbl_df")
+  expect_equal(names(res), c("course_id", "soft_launch_at", "live_at"))
+  expect_true(all(res$soft_launch_at <= res$live_at))
+  expect_equal(length(res$course_id), dplyr::n_distinct(res$course_id))
+  expect_true(nrow(res) >= 15)
+  expect_true(all(!is.na(res$soft_launch_at)))
+  expect_true(all(!is.na(res$live_at)))
+  expect_true(all(!is.na(res$course_id)))
+  expect_true(all(as.difftime(res$live_at - res$soft_launch_at) <
+                    as.difftime(10, unit = "days")))
+
+  first_soft_launches <- soft_launches %>%
+    dplyr::group_by(course_id) %>%
+    dplyr::summarise(first_soft_launch_at = min(soft_launch_at, na.rm = T)) %>%
+    dplyr::collect() %>%
+    inner_join(res, by = "course_id")
+
+  expect_equal(first_soft_launches$first_soft_launch_at, first_soft_launches$soft_launch_at)
+})
+
+test_that("after_join works for out-of-memory tables with mode = inner and type = withingap with numeric", {
+
+  res <- after_join(soft_launches,
+                    hard_launches,
+                    by_user = "course_id",
+                    by_time = c("soft_launch_at" = "live_at"),
+                    type = "withingap",
+                    dt = 1296000) %>%
+    dplyr::collect()
+
+  expect_is(res, "tbl_df")
+  expect_equal(names(res), c("course_id", "soft_launch_at", "live_at"))
+  expect_true(all(res$soft_launch_at <= res$live_at))
+  expect_equal(length(res$course_id), dplyr::n_distinct(res$course_id))
+  expect_true(nrow(res) >= 25)
+  expect_true(all(!is.na(res$soft_launch_at)))
+  expect_true(all(!is.na(res$live_at)))
+  expect_true(all(!is.na(res$course_id)))
+  expect_true(all(as.difftime(res$live_at - res$soft_launch_at) < 1296000))
+
+  first_soft_launches <- soft_launches %>%
+    dplyr::group_by(course_id) %>%
+    dplyr::summarise(first_soft_launch_at = min(soft_launch_at, na.rm = T)) %>%
+    dplyr::collect() %>%
+    inner_join(res, by = "course_id")
+
+  expect_equal(first_soft_launches$first_soft_launch_at, first_soft_launches$soft_launch_at)
+})
+
+
+
