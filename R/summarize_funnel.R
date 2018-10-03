e290ab8f959be74ae16b0cc1efd735dc086a9342
Emily Robinson <emily@datacamp.com>
2018-10-03 12:52:17 -0400

    add (preliminary) summarize conversions

--- R/summarize_funnel.R
+++ R/summarize_funnel.R
@@ -1,6 +1,14 @@
+#' Summarise after join funnel with proportion test
+#'
+#' @param x a data.frame
+#' @param ...
+#' @param ungroup whether the table needs to be ungrouped
+#'
+#' @return a data.frame with proportion test results
+#' @export
 summarize_prop_tests <- function(x, ..., ungroup = TRUE) {
   if (nrow(x) > 2 && is.null(dplyr::groups(x))) {
-    stop("Need to group_by (probably by name column)")
+    stop("Need to group_by (probably by alternative.name column)")
   }
   if (!('alternative.name' %in% colnames(x))) {
     stop("Need a column called alternative.name")
@@ -29,6 +37,24 @@ summarize_prop_tests <- function(x, ..., ungroup = TRUE) {
            pct_change_low = -conf.high / control,
            pct_change_high = -conf.low / control) %>%
     dplyr::select(-conf.low, -conf.high)
+
+  ret
+}
+
+#' Summarize Left-joined table into conversion count
+#'
+#' @param x
+#'
+#' @return a table with three columns, `nb_starts`, `nb_conversions`, and `alternative.name`
+#' @export
+summarize_conversions <- function(x) {
+
+  ret <- x %>%
+    dplyr::count(alternative.name, !is.na(timestamp.y)) %>%
+    dplyr::add_count(alternative.name, wt = n) %>%
+    dplyr::rename(nb_starts = nn, nb_conversions = n) %>%
+    dplyr::filter(`!is.na(timestamp.y)`) %>%
+    dplyr::select(-`!is.na(timestamp.y)`)
 
   ret
 }
