159dcbdd50c45e701bf41cda9525e70347045e9c
Emily Robinson <emily@datacamp.com>
2018-10-16 15:40:34 -0400

    fix name warnings

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 globalVariables(c("..rank", "..time_diff", "estimate1", "estimate2",
                   "p.value", "conf.low", "conf.high", "row_number",
                   "..idx", "..idy", "alternative.name", "treatment",
-                  "control", "."))
+                  "control", ".", "timestamp.y", "COUNT"))
--- R/summarize_funnel.R
+++ R/summarize_funnel.R
@@ -28,7 +28,7 @@ summarize_prop_tests <- function(x, ..., ungroup = TRUE) {
     dplyr::do(broom::tidy(stats::prop.test(.$nb_conversions,
                                            .$nb_starts,
                                            conf.level = .9, ...))) %>%
-    transmute(estimate1,
+    dplyr::transmute(estimate1,
               estimate2,
               p_value = p.value,
               conf.low,
@@ -46,33 +46,33 @@ summarize_prop_tests <- function(x, ..., ungroup = TRUE) {
            pct_change_low = -conf.high / estimate1,
            pct_change_high = -conf.low / estimate1) %>%
     dplyr::select(-conf.low, -conf.high) %>%
-    rename(!!vars)
+    dplyr::rename(!!vars)
 
   ret
 }
 
 #' Summarize Left-joined table into conversion count
 #'
-#' @param x a table with one row per user
+#' @param x a data.frame with one row per user
 #' @param time_col_y the name of the second event time column
 #'
 #' @return a table with three columns, `nb_starts`, `nb_conversions`, and `alternative.name`
 #' @export
 summarize_conversions <- function(x, time_col_y = timestamp.y) {
 
-  var_enq <- enquo(time_col_y)
+  var_enq <- dplyr::enquo(time_col_y)
 
   if (inherits(x, "tbl_lazy")) {
     ret <- x %>%
       dplyr::group_by(alternative.name, add = TRUE) %>%
-      dplyr::summarise(nb_starts = n(),
+      dplyr::summarise(nb_starts = dplyr::n(),
                        nb_conversions = COUNT(!!var_enq))
   }
 
   else {
     ret <- x %>%
       dplyr::group_by(alternative.name, add = TRUE) %>%
-      dplyr::summarise(nb_starts = n(),
+      dplyr::summarise(nb_starts = dplyr::n(),
                        nb_conversions = sum(!is.na(!!var_enq)))
   }
 
