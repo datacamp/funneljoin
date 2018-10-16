2015773f761dbeeba10369a6b5a16a6e56fadc79
Emily Robinson <emily@datacamp.com>
2018-10-16 16:37:54 -0400

    add n to global variable

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,4 @@
 globalVariables(c("..rank", "..time_diff", "estimate1", "estimate2",
                   "p.value", "conf.low", "conf.high", "row_number",
                   "..idx", "..idy", "alternative.name", "treatment",
-                  "control", ".", "timestamp.y", "COUNT"))
+                  "control", ".", "timestamp.y", "COUNT", "n"))
