4bd74a187a51bc3eeb34359b518bc4e0e61ab4f9
Emily <robinson.es@gmail.com>
2019-05-22 17:30:22 -0400

    fix summarize_conversions so won't return NA

--- R/summarize_funnel.R
+++ R/summarize_funnel.R
@@ -86,7 +86,7 @@ summarize_conversions <- function(x, converted) {
   else {
     ret <- x %>%
       dplyr::summarise(nb_starts = dplyr::n(),
-                       nb_conversions = sum(!is.na(!!var_converted) & as.logical(!!var_converted)))
+                       nb_conversions = sum(!is.na(!!var_converted) | is.logical(!!var_converted)))
   }
 
   ret
