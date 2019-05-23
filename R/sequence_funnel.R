6aad0cd14f2dbdb5e523f16710109f96a1b02332
Emily <robinson.es@gmail.com>
2019-05-23 10:33:21 -0400

    add skeleton of sequence funnel functions

---
+++ R/sequence_funnel.R
@@ -0,0 +1,35 @@
+#' Title
+#'
+#' @param type
+#' @param tstamp
+#' @param user_id
+#'
+#' @return
+#' @export
+#'
+#' @examples
+funnel_start <- function(type, tstamp, user_id) {
+
+}
+
+#' Title
+#'
+#' @param type
+#'
+#' @return
+#' @export
+#'
+#' @examples
+funnel_step <- function(type) {
+
+}
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+funnel_end <- function() {
+
+}
