a0b0aa2c47b593a17f3ee13392ddf3904ad25dc4
Emily <robinson.es@gmail.com>
2019-05-29 15:58:51 -0400

    refactor funnel_start, get funnel_step

--- R/sequence_funnel.R
+++ R/sequence_funnel.R
@@ -1,10 +1,16 @@
-tbl_views_snowplow_moments() %>%
-  filter(tstamp > "2019-05-22") %>%
-  funnel_start("registration", moment_type, tstamp, user_id)
-  funnel_step("exercise_start", type = "first-firstafter")
+if (FALSE) {
+  library(dplyr)
+  library(datacampr)
+  tbl_views_snowplow_moments() %>%
+    filter(tstamp > "2019-05-22") %>%
+    funnel_start("registration", moment_type, tstamp, user_id) %>%
+    funnel_step("exercise_start", type = "first-firstafter")
+
+
   funnel_step("project_start", type = "first-firstafter") %>%
-  funnel_step("subscription", type = "first-firstafter") %>%
-  funnel_end()
+    funnel_step("subscription", type = "first-firstafter") %>%
+    funnel_end()
+}
 
 #' Start a funnel
 #'
@@ -19,18 +25,19 @@ tbl_views_snowplow_moments() %>%
 #'
 #' @examples
 funnel_start <- function(tbl, event_type, event, tstamp, user) {
-  attr(tbl, "original_data") <- tbl
-  attr(tbl, "tstamp") <- dplyr::enquo(tstamp)
-  attr(tbl, "user") <- dplyr::enquo(user)
-  attr(tbl, "event") <- dplyr::enquo(event)
-  attr(tbl, "event_sequence") <- event_type
+  md <- list(
+    original_data = tbl,
+    tstamp = dplyr::quo_name(dplyr::enquo(tstamp)),
+    user = dplyr::quo_name(dplyr::enquo(user)),
+    event = dplyr::quo_name(dplyr::enquo(event)),
+    event_sequence = event_type
+  )
 
-  quo_event <- attr(tbl, "event")
+  attr(tbl, "funnel_metadata") <- md
 
   tbl %>%
-    dplyr::filter(!!quo_event == event_type) %>%
-    dplyr::rename_at(vars(-!!enquo(user)), paste0, "_", event_type)
-
+    dplyr::filter(!!dplyr::sym(md$event) == event_type) %>%
+    dplyr::rename_at(vars(-!!md$user), paste0, "_", event_type)
 }
 
 #' Title
@@ -43,22 +50,31 @@ funnel_start <- function(tbl, event_type, event, tstamp, user) {
 #'
 #' @examples
 funnel_step <- function(tbl, event_type, type) {
+  md <- attr(tbl, "funnel_metadata")
+  last_event <- utils::tail(md$event_sequence, 1)
+  md$event_sequence <- c(tbl$event_sequence, event_type)
 
-  quo_event <- attr(tbl, "event")
+  second_event_data <- md$original_data %>%
+    dplyr::filter(!!dplyr::sym(md$event) == event_type) %>%
+    dplyr::rename_at(vars(-!!md$user), paste0, "_", event_type)
 
-  second_event_data <- attr(tbl, "original_data") %>%
-    dplyr::filter(!!quo_event == event_type)
+  tstamp_by <- stats::setNames(paste0(md$tstamp, "_", event_type),
+                               paste0(md$tstamp, "_", last_event))
 
-  tbl %>%
+  ret <- tbl %>%
     after_left_join(second_event_data,
-                    by_user = quo_name(attr(tbl, "user")),
-                    by_time = quo_name(attr(tbl, "tstamp")),
+                    by_user = md$user,
+                    by_time = tstamp_by,
                     type = type)
 
+  attr(ret, "funnel_metadata") <- md
+
+  ret
 }
 
 #' Title
 #'
+#' @param tbl
 #' @return
 #' @export
 #'
