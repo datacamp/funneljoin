8f940a343f03fb17d1638c65ea634dc96df5addb
Emily <robinson.es@gmail.com>
2019-05-30 10:47:21 -0400

    document sequence funnel correctly

--- R/sequence_funnel.R
+++ R/sequence_funnel.R
@@ -1,28 +1,13 @@
-if (FALSE) {
-  library(dplyr)
-  library(datacampr)
-  tbl_views_snowplow_moments() %>%
-    filter(tstamp > "2019-05-22") %>%
-    funnel_start("registration", moment_type, tstamp, user_id) %>%
-    funnel_step("exercise_start", type = "first-firstafter")
-
-
-  funnel_step("project_start", type = "first-firstafter") %>%
-    funnel_step("subscription", type = "first-firstafter")
-}
-
 #' Start a funnel
 #'
-#' @param tbl A table
+#' @param tbl A table of different events and timestamps
 #' @param event_type The first event in the funnel
 #' @param event The name of the column with the event_type
 #' @param tstamp The name of the column with the timestamps of the events
 #' @param user The name of the column indicating the user who did the event
 #'
-#' @return
 #' @export
 #'
-#' @examples
 funnel_start <- function(tbl, event_type, event, tstamp, user) {
   md <- list(
     original_data = tbl,
@@ -36,18 +21,16 @@ funnel_start <- function(tbl, event_type, event, tstamp, user) {
 
   tbl %>%
     dplyr::filter(!!dplyr::sym(md$event) == event_type) %>%
-    dplyr::rename_at(vars(-!!md$user), paste0, "_", event_type)
+    dplyr::rename_at(dplyr::vars(-!!md$user), paste0, "_", event_type)
 }
 
-#' Title
+#' Continue to funnel
 #'
-#' @param event_type
-#' @param type
-#'
-#' @return
+#' @param tbl A table of different events and timestamps
+#' @param event_type The next event in the funnel
+#' @param type The type of after_join (e.g. "first-first", "any-any")
 #' @export
 #'
-#' @examples
 funnel_step <- function(tbl, event_type, type) {
   md <- attr(tbl, "funnel_metadata")
   last_event <- utils::tail(md$event_sequence, 1)
@@ -55,7 +38,7 @@ funnel_step <- function(tbl, event_type, type) {
 
   second_event_data <- md$original_data %>%
     dplyr::filter(!!dplyr::sym(md$event) == event_type) %>%
-    dplyr::rename_at(vars(-!!md$user), paste0, "_", event_type)
+    dplyr::rename_at(dplyr::vars(-!!md$user), paste0, "_", event_type)
 
   tstamp_by <- stats::setNames(paste0(md$tstamp, "_", event_type),
                                paste0(md$tstamp, "_", last_event))
