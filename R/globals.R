c32d6fc781011f1c2f0a74236c1b93dcc6b3545f
Emily <robinson.es@gmail.com>
2019-07-12 14:08:05 -0400

    add moment_type, nb_step to globals

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,5 @@
 globalVariables(c("..rank", "..time_diff", "estimate1", "estimate2",
                   "p.value", "conf.low", "conf.high", "row_number",
                   "..idx", "..idy", "alternative.name", "treatment",
-                  "control", ".", "timestamp.y", "COUNT", "n", ".gap"))
+                  "control", ".", "timestamp.y", "COUNT", "n", ".gap",
+                  "moment_type", "nb_step"))
